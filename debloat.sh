d6a654004e2ac9254a7dfa6cb4a416d405f896b6
HiTechNishachar <79357474+HiTechNishachar@users.noreply.github.com>
2023-10-23 17:02:13 +0530

    Add Gnome debloat

--- debloat.sh
+++ debloat.sh
@@ -7,6 +7,10 @@ echo "..........DEBLOAT.........."
 echo "Remove ALL the KDE PIM packages : "
 sudo apt purge akonadi* -y && sudo apt autoremove -y
 
+echo "Remove ALL the Gnome-Games "
+sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex -y & sudo apt autoremove -y
+
+
 echo "Remove the FIREFOX ESR version , Konqueror and Transmission : "
 sudo apt purge firefox-esr konqueror transmission -y && sudo apt autoremove -y
 
